a6e308f1a37c73100220ea90aaa31ec1d3cb11eb
alpinetyler <49990880+alpinetyler@users.noreply.github.com>
2021-07-05 14:21:58 -0600

    changes to Login component

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,17 +20,16 @@ export default class Body extends Component {
                     commercial or ATP pilot, we have the resources to keep you current!</p>
                 <p>
                     Feel free to browse our free areas to your heart's content. You'll find questions,
-                    quizzes and answers and explanations for all of the things you once learned and may have
+                    quizzes, answers and explanations for things you once learned and may have
                     forgotten. We're all about knocking off the rust and getting you current again! Our resources
                     are also great for those who are current and want to stay on top of their game.
-                <p>
+                <p><br />
                     Do you want to get ready for that flight review, or perhaps you're preparing for an
                     initial or re-current check-ride or written exam? Our resources are appropriate for everyone
                     who wants to learn about or re-learn the most exciting subject possible -- aviation!
                 </p>
                 </p>
                 </div>
-                <Form />
             </div>
         )
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,19 @@ function Login() {
   return (
     <div className="App">
         <h1 className="heading">Login</h1>
+        <form action="/Body">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" autocomplete="off" placeholder="name@example.com" />
+                <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Password</label>
+                
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+        
     </div>
   );
 }
